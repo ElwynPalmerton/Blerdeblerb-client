890cd8b89eb8078bcaf9714824929e985b38f109
Elwyn Palmerton <elwyn.palmerton@gmail.com>
2022-11-15 10:40:02 -0800

    modify baseUrl

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 
 // const herokuURL="https://shielded-dusk-53756.herokuapp.com/"
-
 // export const baseURL = "https://shielded-dusk-53756.herokuapp.com/"
-export const baseURL = "http://localhost:5000";
+
+const awsUrl =
+  "http://blerdeblergapi-env.eba-i3httipm.us-west-1.elasticbeanstalk.com";
+
+const localUrl = "http://localhost:5001";
+export const baseURL =
+  "http://blerdeblergapi-env.eba-i3httipm.us-west-1.elasticbeanstalk.com";
 
 // export const baseURL = "http://localhost:9000";
 
