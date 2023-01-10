b08a6cde9f25229bde23fcde5f1e64eafe373557
Elwyn Palmerton <elwyn.palmerton@gmail.com>
2023-01-10 12:39:00 -0800

    clean-up api config

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,14 +1,11 @@
 import axios from "axios";
 
 const config = {
-  // ! This should be 8080 - doublecheck
   development: "http://localhost:8080",
   production: "https://blerdeblurb-api.com",
   qa: "https://blerdeblurb-api.com", // Run UI locally with live API.
-  docker: "http://localhost:5001", // not implemented yet.
+  docker: "http://localhost:5001", // Run locally with Docker
 };
-
-// should reference container name...
 
 const environment = process.env.REACT_APP_ENV;
 export const baseURL = config[environment];
